5e5feb9ce5a89c045c30f3622ff73c436391e41b
Pliteweq <140956668+Pliteweq@users.noreply.github.com>
2023-07-30 16:30:39 +0300

    Update creds.py

--- creds.py
+++ creds.py
@@ -3,6 +3,6 @@ TWITCH_TOKEN = "zdzghayeacyk6f7axxs6fvksi41u46"
 # Your TWITCH Channel Name
 TWITCH_CHANNEL = "pliteweq"
 # Your OpenAI API Key
-OPENAI_API_KEY = "gp762nuuoqcoxypju8c569th9wz7q5"
+OPENAI_API_KEY = "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"
 # Your Google Cloud JSON Path
 GOOGLE_JSON_PATH = ""
