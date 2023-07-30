4502a09cb709bcf6459896a18b4b9ea9750b33b9
Pliteweq <140956668+Pliteweq@users.noreply.github.com>
2023-07-30 16:16:56 +0300

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,8 +1,8 @@
 # You're Twitch Token 
-TWITCH_TOKEN = ""
+TWITCH_TOKEN = "zdzghayeacyk6f7axxs6fvksi41u46"
 # Your TWITCH Channel Name
-TWITCH_CHANNEL = ""
+TWITCH_CHANNEL = "pliteweq"
 # Your OpenAI API Key
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = "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"
 # Your Google Cloud JSON Path
 GOOGLE_JSON_PATH = ""
